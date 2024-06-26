8879f3011ae4bb3f7fdfc7fa713a7c3725ef78a0
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-26 12:30:10 -0300

    null

--- src/movies/links.ts
+++
@@ -1,5 +0,0 @@
-export const KEY = "api_key=28c126fdec6d5cc97c09116813740164";
-export const LINK = "https://api.themoviedb.org/3/movie/";
-export const SEARCH = "https://api.themoviedb.org/3/trending/movie/";
-export const TRENDING = "https://api.themoviedb.org/3/trending/movie/day";
-export const IMAGE = "https://image.tmdb.org/t/p/w500/";
