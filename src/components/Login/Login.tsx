dab77734656776baaaadadd93082925b2bb450cf
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-27 10:07:53 -0300

    created the login component structure and created function to redirect to the home route on form submit

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,34 +1,32 @@
-import { Link } from "react-router-dom";
+import { FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./login.module.css";
 
 export function Login() {
+  const navigate = useNavigate();
+
+  const handleUserLogin = (event: FormEvent) => {
+    event.preventDefault();
+    navigate("/home");
+  };
+
   return (
-    <section className={styles.loginPage}>
-      <div className={styles.loginDiv}>
-        <p className={styles.loginHeading}>Login</p>
-        <form>
-          <input
-            type="email"
-            name="email"
-            className={styles.inputLogin}
-            placeholder="Email adress"
-            autoComplete="off"
-          />
-          <input
-            type="password"
-            name="password"
-            className={styles.inputLogin}
-            placeholder="Your password"
-            autoComplete="off"
-          />
-          <button type="submit" className={styles.loginButton}>
-            Login to your account
-          </button>
-          <p className={styles.linkSignup}>
-            Do not have an account? <Link to="/signup">Sign Up</Link>
-          </p>
-        </form>
-      </div>
-    </section>
+    <form className={styles.loginForm} onSubmit={(e) => handleUserLogin(e)}>
+      <p className={styles.loginHeading}>Login</p>
+      <input type="text" name="email" className={styles.inputLogin} placeholder="Email adress" autoComplete="off" />
+      <input
+        type="password"
+        name="password"
+        className={styles.inputLogin}
+        placeholder="Your password"
+        autoComplete="off"
+      />
+      <button type="submit" className={styles.loginButton}>
+        Login to your account
+      </button>
+      <p className={styles.linkSignup}>
+        Do not have an account? <Link to="/signup">Sign Up</Link>
+      </p>
+    </form>
   );
 }
