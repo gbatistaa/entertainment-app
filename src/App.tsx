e49692de38ca8e46608704421b023e2c11536d5e
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-27 09:54:48 -0300

    added signup react component and its route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
+import { Signup } from "./components/Signup/Signup";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
