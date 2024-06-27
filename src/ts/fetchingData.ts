b3ba9622ee38d4ba270d889210aef5848676d73f
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-27 10:10:29 -0300

    deleted files

--- src/ts/fetchingData.ts
+++
@@ -1,9 +0,0 @@
-export const fetchingMovies = async (moviesUrl: string, apiKey: string) => {
-  try {
-    const data = await fetch(`${moviesUrl}?${apiKey}`);
-    const response = await data;
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
-};
