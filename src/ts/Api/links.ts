9ad52388512dfb87beac3dcceba755183496fec8
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-27 09:45:53 -0300

    changing on the folder name and added some more base links

---
+++ src/ts/Api/links.ts
@@ -0,0 +1,7 @@
+export const KEY = "api_key=28c126fdec6d5cc97c09116813740164";
+export const MOVIE = "https://api.themoviedb.org/3/movie/";
+export const SEARCH = "https://api.themoviedb.org/3/trending/movie/";
+export const TRENDING = "https://api.themoviedb.org/3/trending/movie/day";
+export const IMAGE = "https://image.tmdb.org/t/p/w500/";
+export const GENRES = `https://api.themoviedb.org/3/genre/movie/list`;
+export const MOVIES_BY_GENRE = `https://api.themoviedb.org/3/discover/movie?${KEY}&with_genres=`;
