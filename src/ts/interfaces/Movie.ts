590756ec451f20b95ac657f8212bb008793b3afe
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-27 09:52:56 -0300

    added files of interfaces and validation functions

---
+++ src/ts/interfaces/Movie.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  readonly title: string;
+  readonly vote_average: number;
+  readonly release_date: string;
+  readonly overview: string;
+  readonly poster_path: string;
+  isMovie: boolean;
+}
---
+++ src/ts/interfaces/MovieCardType.ts
@@ -0,0 +1,5 @@
+import { Movie } from "../interfaces/Movie";
+
+export interface MovieCardType {
+  moviesArray: Movie[];
+}
