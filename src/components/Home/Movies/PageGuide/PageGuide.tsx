5943c6562b5b3fc47a7b314fc8f159790cd5b876
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-07-02 16:30:39 -0300

    realocation of the page guide element to a separeted react component

--- src/components/Home/Movies/PageGuide/PageGuide.tsx
+++ src/components/Home/Movies/PageGuide/PageGuide.tsx
@@ -8,40 +8,42 @@ import styles from "./pageGuide.module.css";
 
 export function PageGuide() {
   return (
-    <div className={styles.pageGuide}>
-      <Link to={"/home"}>
-        <MdMovie size={30} className={styles.moviesLogo} />
-      </Link>
-      <nav className={styles.pagesNavigate}>
-        <ul>
-          <li>
-            <Link to={"/home/main"}>
-              <IoGrid size={20} className={styles.navIcons} />
-            </Link>
-          </li>
+    <section className={styles.pageGuideContainer}>
+      <div className={styles.pageGuide}>
+        <Link to={"/home"}>
+          <MdMovie size={30} className={styles.moviesLogo} />
+        </Link>
+        <nav className={styles.pagesNavigate}>
+          <ul>
+            <li>
+              <Link to={"/home/main"}>
+                <IoGrid size={20} className={styles.navIcons} />
+              </Link>
+            </li>
 
-          <li style={{ transform: "rotate(90deg)" }}>
-            <Link to={"/home/movies"}>
-              <PiFilmStripFill size={20} className={styles.navIcons} />
-            </Link>
-          </li>
+            <li style={{ transform: "rotate(90deg)" }}>
+              <Link to={"/home/movies"}>
+                <PiFilmStripFill size={20} className={styles.navIcons} />
+              </Link>
+            </li>
 
-          <li>
-            <Link to={"/home/tvseries"}>
-              <PiTelevision size={20} className={styles.navIcons} />
-            </Link>
-          </li>
+            <li>
+              <Link to={"/home/tvseries"}>
+                <PiTelevision size={20} className={styles.navIcons} />
+              </Link>
+            </li>
 
-          <li>
-            <Link to={"/home/bookmarked"}>
-              <FaBookmark size={20} className={styles.navIcons} />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Link to={"/profile"} style={{ marginTop: "auto" }}>
-        <BsPersonCircle size={25} />
-      </Link>
-    </div>
+            <li>
+              <Link to={"/home/bookmarked"}>
+                <FaBookmark size={20} className={styles.navIcons} />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Link to={"/profile"} style={{ marginTop: "auto" }}>
+          <BsPersonCircle size={25} />
+        </Link>
+      </div>
+    </section>
   );
 }
