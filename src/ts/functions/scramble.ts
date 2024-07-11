5befee4a548b775d0e0cf574f511ecebaebddb7f
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-07-11 09:13:00 -0300

    added the main logic of the scramble array elements function

--- src/ts/functions/scramble.ts
+++ src/ts/functions/scramble.ts
@@ -3,5 +3,13 @@ import { TvSeries } from "../interfaces/TvSeries";
 
 export const scrambleMoviesWithSeries = (moviesArray: Movie[], seriesArray: TvSeries[]) => {
   const scrambledArray: (Movie | TvSeries)[] = [...moviesArray, ...seriesArray];
+
+  for (let i = 0; i < scrambledArray.length; i++) {
+    const [firstIndex, secondIndex] = [Math.floor(Math.random() * 40), Math.floor(Math.random() * 40)];
+    const pivot = scrambledArray[firstIndex];
+    scrambledArray[firstIndex] = scrambledArray[secondIndex];
+    scrambledArray[secondIndex] = pivot;
+  }
+
   return scrambledArray;
 };
