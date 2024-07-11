21d72442460de2bbc41bb97f79dcdb2084bac837
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-07-11 09:11:38 -0300

    addition of the tv series url

--- src/ts/Api/links.ts
+++ src/ts/Api/links.ts
@@ -1,5 +1,6 @@
 export const KEY = "28c126fdec6d5cc97c09116813740164";
 export const MOVIE = "https://api.themoviedb.org/3/movie/";
+export const TVSERIES = "https://api.themoviedb.org/3/tv/top_rated?";
 export const TRENDING = "https://api.themoviedb.org/3/trending/movie/day";
 export const IMAGE = "https://image.tmdb.org/t/p/w780";
 export const GENRES = `https://api.themoviedb.org/3/genre/movie/list`;
