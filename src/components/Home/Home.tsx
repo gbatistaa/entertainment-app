fdcabe8a187266a6c8fd6d28956bae1e6bd87233
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-07-11 08:12:22 -0300

    added the movie search structure and transposition of the content grid to another section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,30 @@
 import styles from "./home.module.css";
 
+import { IoSearch } from "react-icons/io5";
+
 import { PageGuide } from "./Movies/PageGuide/PageGuide";
-import { TrendingMovies } from "./Movies/Trending/Trending";
+import { Recommended } from "./Movies/Recommended/Recommended";
+import { Trending } from "./Movies/Trending/Trending";
 
 export function Home() {
   return (
     <div className={styles.home}>
       <PageGuide />
       <main className={styles.homePage}>
-        <TrendingMovies />
+        <label className={styles.movieInputContainer}>
+          <IoSearch size={25} />
+          <input
+            type="text"
+            placeholder="Search for Movies or TV series"
+            name="movie"
+            className={styles.movieInputSearch}
+            autoComplete="off"
+          />
+        </label>
+        <section className={styles.moviesGrid}>
+          <Trending />
+          <Recommended />
+        </section>
       </main>
     </div>
   );
