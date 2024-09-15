914665d036a53e665f0d3281d9664de772196f6c
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-09-15 18:14:28 -0300

    added routing on movies grid, and added bookmarked section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Home } from "./components/Home/Home";
+import { MainPage } from "./components/Home/Main/MainPage";
+import { MoviesGrid } from "./components/Home/Routes/MainGrid/Main/MoviesGrid";
+import { TvSeries } from "./components/Home/Routes/TV/TvSeries";
 import { Login } from "./components/Login/Login";
 import { Signup } from "./components/Signup/Signup";
 
@@ -12,7 +14,26 @@ function App() {
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<MainPage />}>
+            <Route path="/home" element={<MoviesGrid />} />
+            <Route path="/home/tvseries" element={<TvSeries />} />
+            <Route
+              path="/home/movies"
+              element={
+                <div>
+                  <h1>Movies</h1>
+                </div>
+              }
+            />
+            <Route
+              path="/home/bookmarked"
+              element={
+                <div>
+                  <h1>Bookmarked</h1>
+                </div>
+              }
+            />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
