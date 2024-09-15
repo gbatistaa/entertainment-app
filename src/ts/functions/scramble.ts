95cb47e948f2d69ffaca7fdf3b8107431b1a3cd9
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-09-15 18:22:45 -0300

    formating

--- src/ts/functions/scramble.ts
+++ src/ts/functions/scramble.ts
@@ -3,9 +3,11 @@ import { TvSeries } from "../interfaces/TvSeries";
 
 export const scrambleMoviesWithSeries = (moviesArray: Movie[], seriesArray: TvSeries[]) => {
   const scrambledArray: (Movie | TvSeries)[] = [...moviesArray, ...seriesArray];
-
   for (let i = 0; i < scrambledArray.length; i++) {
-    const [firstIndex, secondIndex] = [Math.floor(Math.random() * 40), Math.floor(Math.random() * 40)];
+    const [firstIndex, secondIndex] = [
+      Math.floor(Math.random() * scrambledArray.length - 1),
+      Math.floor(Math.random() * scrambledArray.length - 1),
+    ];
     const pivot = scrambledArray[firstIndex];
     scrambledArray[firstIndex] = scrambledArray[secondIndex];
     scrambledArray[secondIndex] = pivot;
