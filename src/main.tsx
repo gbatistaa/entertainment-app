88804587e341b6a567dc8ec9c306daf79c5a242f
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-09-15 18:15:02 -0300

    added swiper library imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { register } from "swiper/element/bundle";
 import App from "./App.tsx";
 import "./index.css";
-
+register();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
